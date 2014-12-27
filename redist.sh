f4718ff19c853e5f25da0d7accb77e518e0da952
Axel Isouard <a.isouard@dotstar.fr>
2014-12-27 19:05:53 +0100

    Add redist script

---
+++ redist.sh
@@ -0,0 +1,10 @@
+rm -rf redist_release redist_debug
+
+mkdir -p redist_release/lib redist_release/include redist_release/bin
+mkdir -p redist_debug/lib redist_debug/include redist_debug/bin
+
+find out/Release -name *.a -exec cp {} redist_release/lib/ \;
+find out/Release -executable -type f -exec cp {} redist_release/bin/ \;
+
+find out/Debug -name *.a -exec cp {} redist_debug/lib/ \;
+find out/Debug -executable -type f -exec cp {} redist_debug/bin/ \;
