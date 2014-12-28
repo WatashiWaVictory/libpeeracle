1f9b30e05104e08921b5974cbf50dee89fc5569a
Buildbot <buildbot@dotstar.fr>
2014-12-27 23:32:02 -0800

    Fixed redist scripts

---
+++ redist.sh
@@ -0,0 +1,5 @@
+rm -rf redist_release
+
+mkdir -p redist_release/lib redist_release/include redist_release/bin
+
+find out/Release -type f ( -name webrtc.lib -or -name webrtc_common.lib -or -name rtc_base.lib -or -name rtc_base_approved.lib -or -name common_audio.lib -or -name common_video.lib -or -name rtc_p2p.lib -or -name rtc_sound.lib -or -name video_engine_core.lib -or -name voice_engine.lib -or -name libjingle_media.lib -or -name libjingle_p2p.lib -or -name libjingle_peerconnection.lib -or -name libwebsockets.lib -or -name peeracle.lib ) -exec mv {} redist_release/lib/ \;
