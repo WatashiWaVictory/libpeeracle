6d5d6b45446a3c512738a73ee97f58b60772df05
Axel Isouard <a.isouard@dotstar.fr>
2015-02-18 12:22:06 +0100

    Fix redist script for OSX

--- redist.sh
+++ redist.sh
@@ -4,4 +4,4 @@ mkdir -p redist_release/lib redist_release/include redist_release/bin
 
 find out/Release -type f \( -name libwebrtc.a -or -name libwebrtc_common.a -or -name librtc_base.a -or -name librtc_base_approved.a -or -name libcommon_audio.a -or -name libcommon_video.a -or -name librtc_p2p.a -or -name librtc_sound.a -or -name libvideo_engine_core.a -or -name libvoice_engine.a -or -name libjingle_media.a -or -name libjingle_p2p.a -or -name libjingle_peerconnection.a -or -name libwebsockets.a -or -name libpeeracle.a \) -exec mv {} redist_release/lib/ \;
 
-find webrtc talk third_party/libwebsockets -name *.h -exec cp --parents {} redist_release/include/ \;
+find peeracle webrtc talk third_party/libwebsockets -name *.h -exec rsync -R {} redist_release/include/ \;
